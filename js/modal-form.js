93a8bcdb455c59f60195e6ee0944764abf748564
Dmitry <dmitry@gnuhost.eu>
2018-07-11 15:37:38 +0300

    ud

--- js/modal-form.js
+++ js/modal-form.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 		event.preventDefault();
 			$('#modal_form')
 				.css('display', 'block')
-				.animate({opacity: 1, top: '75%'}, 200);
+				.animate({opacity: 1, top: '70%'}, 200);
 	});
-	$('#modal_close').click( function(){
+	$('#modal_close, #submit-1').click( function(){
 		$('#modal_form')
 			.animate({opacity: 0, top: '45%'}, 200,
 				function(){
@@ -22,7 +22,7 @@ $(document).ready(function() {
 				.css('display', 'block')
 				.animate({opacity: 1, top: '82%'}, 200);
 	});
-	$('#modal_close-2').click( function(){
+	$('#modal_close-2, #submit-2').click( function(){
 		$('#modal_form-2')
 			.animate({opacity: 0, top: '45%'}, 200,
 				function(){
--- js/prices.js
+++ js/prices.js
@@ -6,13 +6,13 @@ function prices_f_1() {
     var total = 0;
 
     if (amount_sum < 4) {
-        price = 179;
+        price = 149;
         total = amount_sum * price;
     } else if (3 < amount_sum < 7) {
-        price = 169;
+        price = 139;
         total = amount_sum * price;
     } else if (amount_sum > 6) {
-        price = 149;
+        price = 119;
         total = amount_sum * price;
     }
 
@@ -31,13 +31,13 @@ function prices_f_2() {
     var total = 0;
 
     if (amount_sum < 4) {
-        price = 179;
+        price = 149;
         total = amount_sum * price;
     } else if (3 < amount_sum < 7) {
-        price = 169;
+        price = 139;
         total = amount_sum * price;
     } else if (amount_sum > 6) {
-        price = 149;
+        price = 119;
         total = amount_sum * price;
     }
 
